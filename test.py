4086bad44ca63683c8d02a7b4c8a90e72a20477b
Yunha Choi <61413047+choiyhking@users.noreply.github.com>
2024-01-20 16:52:32 +0900

    Update test.py

--- test.py
+++ test.py
@@ -15,7 +15,7 @@ model, decoder, utils = torch.hub.load(repo_or_dir='snakers4/silero-models',
 # download a single file in any format compatible with TorchAudio
 torch.hub.download_url_to_file('https://opus-codec.org/static/examples/samples/speech_orig.wav',
                                dst ='speech_orig.wav', progress=True)
-test_files = glob('LJSpeech-1.1/wavs/*.wav')
+test_files = glob('LJSpeech/wavs/*.wav')
 batches = split_into_batches(test_files, batch_size=10)
 
 total_processing_time = 0.0
